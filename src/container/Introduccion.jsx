4079454772a5b0eec93f9670f564294bd838d866
MateoChacon <xdmateo90@gmail.com>
2023-01-25 22:16:56 -0500

    deleta resumen

--- src/container/Introduccion.jsx
+++ src/container/Introduccion.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import Grupal from "../assets/trabajo-en-equipo-social.jpg";
-import integranteOne from "../assets/integrante-1.jpg";
-import integranteTwo from "../assets/integrante-2.jpg";
-import integranteThree from "../assets/integrante-3.jpg";
-import integranteFour from "../assets/integrante-4.jpg";
 
 const Introduccion = () => {
   return (
@@ -74,26 +70,6 @@ const Introduccion = () => {
             página web puede presentar el contenido de forma concisa y directa
             al usuario.
           </p>
-        </div>
-      </div>
-
-      <div className="flex flex-row items-center my-20  w-full bg-secondary text-center text-white font-serif text-2xl justify-around">
-        <p className="text-8xl mb-10">Integrantes</p>
-        <div>
-          <img className="h-[130px] rounded-3xl" src={integranteOne} alt="" />
-          <p className="my-5">Jhon Godoy</p>
-        </div>
-        <div>
-          <img className="h-[130px] rounded-3xl" src={integranteTwo} alt="" />
-          <p className="my-5">Kevin Avila</p>
-        </div>
-        <div>
-          <img className="h-[130px] rounded-3xl" src={integranteThree} alt="" />
-          <p className="my-5">David Pozo</p>
-        </div>
-        <div>
-          <img className="h-[130px] rounded-3xl" src={integranteFour} alt="" />
-          <p className="my-5 text-center">Mateo Chacon</p>
         </div>
       </div>
     </section>
--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import integranteOne from "../assets/integrante-1.jpg";
+import integranteTwo from "../assets/integrante-2.jpg";
+import integranteThree from "../assets/integrante-3.jpg";
+import integranteFour from "../assets/integrante-4.jpg";
 
 import trabajoGrupal from "../assets/beneficios-trabajo-en-equipo1.jpg";
 
@@ -6,38 +10,23 @@ const Main = () => {
   return (
     <section className="">
       <img className="w-full h-[400px]" src={trabajoGrupal} alt="" />
-      <div className="px-10">
-        <h1 className="font-serif text-3xl text-center my-8 text-red-800">
-          Resumen
-        </h1>
-        <div className="flex flex-col lg:flex-row justify-around">
-          <p className="text-white font-sans w-[600px] mr-5">
-            El siguiente proyecto se trata sobre la parte de una problemática
-            muy común a nivel de empresas, dicha problemática tiene que ver con
-            la gestión y difusión de información de los mismos,
-            independientemente del tamaño de la empresa, esta trata de
-            mantenerse competitiva y sobre todo mantener un nivel de información
-            que le garantice la recuperación de la inversión y lógicamente una
-            ganancia dentro de los rangos permisibles. La propuesta de solución
-            planteada va a consistir en la creación y la implementación de una
-            Página web con lenguaje de programación JavaScript utilizando
-            también un lenguaje de etiquetas HTML y un lenguaje de estilos CSS.
-          </p>
-          <p className="text-white font-sans w-[600px]">
-            El cual va a ser desarrollado para que la información de la empresa
-            sea más accesible y concisa para los clientes a su vez dando
-            información sobre los productos y servicios que da la empresa. Para
-            esto se ha de comenzar haciendo la estructura de la página usando
-            las herramientas de Visual Studio Code, un programa que se utiliza
-            para la creacion de codigos HTML, además de aplicar las reglas de
-            estilo CSS, con la implementación del “media querie” para añadir
-            responsividad a la página, pero para tener una mejor idea de cómo
-            queremos que quede nuestra página web, se utilizará el programa
-            Figma, gracias a este programa podremos diseñar el diseño de como se
-            verá. También se implementará el uso de GitHub, el cual se usará
-            como un medio para compartir los cambios realizados desde diferentes
-            máquinas del código HTML entre los miembros del equipo
-          </p>
+      <div className="flex flex-row items-center my-20  w-full bg-secondary text-center text-white font-serif text-2xl justify-around">
+        <p className="text-8xl mb-10">Integrantes</p>
+        <div>
+          <img className="h-[130px] rounded-3xl" src={integranteOne} alt="" />
+          <p className="my-5">Jhon Godoy</p>
+        </div>
+        <div>
+          <img className="h-[130px] rounded-3xl" src={integranteTwo} alt="" />
+          <p className="my-5">Kevin Avila</p>
+        </div>
+        <div>
+          <img className="h-[130px] rounded-3xl" src={integranteThree} alt="" />
+          <p className="my-5">David Pozo</p>
+        </div>
+        <div>
+          <img className="h-[130px] rounded-3xl" src={integranteFour} alt="" />
+          <p className="my-5 text-center">Mateo Chacon</p>
         </div>
       </div>
     </section>
