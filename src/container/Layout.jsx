da97fd10a8eb3d6ab6d6d2bd652118d0ccc35536
MateoChacon <xdmateo90@gmail.com>
2023-02-01 13:41:24 -0500

    change in page main abaout topic summary

--- src/container/Layout.jsx
+++ src/container/Layout.jsx
@@ -1,32 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import iconReact from "../assets/icons8-react-64.png";
 
 const Layout = ({ children }) => {
   return (
     <div className="bg-secondary min-h-screen">
       <nav className="flex flex-row justify-between bg-primary py-5">
-        <div href="/" className="flex flex-row items-center">
+        <div className="flex flex-row items-center">
           <img className="ml-10" src={iconReact} alt="" />
-          <a href="/" className="text-white font-serif text-2xl ml-2">
+          <Link to="/" className="text-white font-serif text-2xl ml-2">
             Piensa
-          </a>
+          </Link>
         </div>
         <div className="flex flex-row items-center justify-between">
-          <a
-            href="/Introduccion"
+          <Link
+            to="/Introduccion"
             className="text-white font-serif text-xl ml-10"
           >
             Introduccion
-          </a>
-          <a
-            href="/MarcoDeReferencia"
+          </Link>
+          <Link
+            to="/MarcoDeReferencia"
             className="text-white font-serif text-xl ml-10"
           >
             Marco de Referencia
-          </a>
-          <a href="/Encuesta" className="text-white font-serif text-xl mx-10">
+          </Link>
+          <Link to="/Encuesta" className="text-white font-serif text-xl mx-10">
             Encuesta
-          </a>
+          </Link>
         </div>
       </nav>
       <div className="bg-secondary">{children}</div>
--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -10,6 +10,57 @@ const Main = () => {
   return (
     <section className="">
       <img className="w-full h-[400px]" src={trabajoGrupal} alt="" />
+      <div>
+        <h1 className="text-center text-8xl font-serif text-red-700">
+          Resumen
+        </h1>
+        <div className="flex flex-row text-white px-10">
+          <div className=" w-2/4 mr-2">
+            El siguiente proyecto se trata sobre la parte de una problemática
+            muy común a nivel de empresas, dicha problemática tiene que ver con
+            la gestión y difusión de información de los mismos,
+            independientemente del tamaño de la empresa, esta trata de
+            mantenerse competitiva y sobre todo mantener un nivel de información
+            que le garantice la recuperación de la inversión y lógicamente una
+            ganancia dentro de los rangos permisibles. La propuesta de solución
+            planteada va a consistir en la creación y la implementación de una
+            Página web con lenguaje de programación JavaScript utilizando
+            también un lenguaje de etiquetas HTML y un lenguaje de estilos CSS,
+            el cual va a ser desarrollado para que la información de la empresa
+            sea más accesible y concisa para los clientes a su vez dando
+            información sobre los productos y servicios que da la empresa. Para
+            esto se ha de comenzar haciendo la estructura de la página usando
+            las herramientas de Visual Studio Code, un programa que se utiliza
+            para la creación de códigos HTML, además de aplicar las reglas de
+            estilo CSS, con la implementación del “media querie” para añadir
+            responsividad a la página, pero para tener una mejor idea de cómo
+            queremos que quede nuestra página web, se utilizará el programa
+            Figma, gracias a este programa podremos diseñar el cómo se verá.
+            También se implementó el uso de GitHub, el cual es un medio usado
+            para compartir los cambios realizados desde diferentes máquinas del
+            código HTML entre los miembros del equipo.
+          </div>
+          <div className=" w-2/4 ml-2">
+            Tema: <br /> -El tema a investigar es sobre la empresa Estrual y los
+            servicios que ofrece, como también en cómo le afecta no tener una
+            página web oficial que automatice sus servicios de forma virtual y
+            concisa. <br /> Problemática: <br /> -No ha tenido una página web,
+            desde el momento en que entró en funcionamiento, por eso se está
+            creando una página web, para poder automatizar los servicios que
+            ofrece la empresa, de forma presencial o no presencial. <br />{" "}
+            Objetivo general: <br />
+            -Desarrollar una página web informativa para la empresa Estrual
+            desarrollada por lenguaje de programación JavaScript para
+            automatizar los servicios de la empresa. <br /> Objetivo específico{" "}
+            <br />
+            -Implementar un servicio automatizado para la empresa. Metodología
+            -La página web será desarrollada mediante o siguiendo las bases de
+            la metodología scrum. Resultados esperados: -Desarrollar destreza y
+            habilidad mínimas con el lenguaje de programación JavaScript.
+            Palabras clave -Página web, Empresa, Cuenca, Lenguaje JavaScript.
+          </div>
+        </div>
+      </div>
       <div className="flex flex-row items-center my-20  w-full bg-secondary text-center text-white font-serif text-2xl justify-around">
         <p className="text-8xl mb-10">Integrantes</p>
         <div>
